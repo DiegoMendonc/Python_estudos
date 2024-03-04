fdc0cc5c7a3b68d77970f61c9168d4d6c0f0d5f0
DiegoMendonc <diego_felipe_g.m@live.com>
2024-03-04 20:22:06 -0300

    arquivos criados

---
+++ DIO/conhecendo_a_linguagem.py
@@ -0,0 +1,16 @@
+
+print("-"*25)
+print("\033[5;35mTipos de Dados\033[m")
+print("-"*25)
+print("Números:\n")
+n = int(input("Digite um número: "))
+n1 = int(input("Digite o segundo número: "))
+n2 = n + n1
+print(f"\n\033[5;36;44m{n} + {n1} = {n2}\033[m\n")
+print("-"*25)
+print("\n Float: \n")
+n3 = float(input("Digite um número: "))
+n4 = float(input("Digite o segundo número: "))
+n5 = n3 + n4
+print(f"\n{n3} + {n4} = {n5}\n")
+print("-"*25)
---
+++ DIO/primeiro_programa.py
@@ -0,0 +1,1 @@
+print("\033[3;3;45mHello World!\033[m")
