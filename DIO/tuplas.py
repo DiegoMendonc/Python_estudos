adf38b17252a3c35fbeb148d98ba3f5be8e817f7
DiegoMendonc <diego_felipe_g.m@live.com>
2024-03-09 10:42:19 -0300

    Update tuplas.py

--- DIO/tuplas.py
+++ DIO/tuplas.py
@@ -8,4 +8,28 @@ def esp():
     print()
 
 menu("TUPLAS em Python")
+print("""      
+Tuplas são idênticas na execução e manuseio com as listas, PORÉM, 
+as tuplas são imutáveis, limitando muito o manuseio da tupla
+""")
+frutas = ("laranja", "maca", "uva")
+print(frutas)
+esp()
 
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+esp()
+
+pais = ("Brasil", )
+print(pais)
+
+menu("Utilizando as tuplas")
+
+print(frutas)
+print()
+
+print(frutas[::-1])
+esp()
+
+print(frutas[2])
+esp()
