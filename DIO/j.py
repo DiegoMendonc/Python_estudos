35f0d36d215f5df39d02dca0112c0a2a5d240f9b
DiegoMendonc <diego_felipe_g.m@live.com>
2024-03-10 21:41:34 -0300

    Update j.py

--- DIO/j.py
+++ DIO/j.py
@@ -1,6 +1,29 @@
-capacidade_atual, aumento_percentual = int(input())
-print(capacidade_atual, aumento_percentual)
+# def mi(a, b):
+   # a = a
+    # b = b / 100
+    # return (a + (a * b))
+
+
+# capacidade_atual, aumento_percentual = int(input("Digite um valor"))
+
+# mi(capacidade_atual, aumento_percentual)
 
 #// TODO: Calcule a nova capacidade do disco de Mithril
 
 #// TODO: Imprima a nova capacidade
+
+#def map(int, b)
+   # int = 
+#capacidade_atual = int(input(".")) 
+#aumento_percentual = int(input("."))
+
+#def map(a, b):
+ #   global capacidade_atual, aumento_percentual
+    
+  #  a = a
+   ##print (int(a + (a * b)))
+
+#map(capacidade_atual, aumento_percentual)
+
+capacidade_atual, aumento_percentual = map(int, input(".").split())
+print(int(capacidade_atual + (capacidade_atual * (aumento_percentual / 100))))
