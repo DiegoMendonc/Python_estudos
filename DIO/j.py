bebcc56c8f1944ca619f58b5e5f9642a55b4b06a
DiegoMendonc <diego_felipe_g.m@live.com>
2024-04-11 13:05:57 -0300

    atualizações de estudos - python

--- DIO/j.py
+++ DIO/j.py
@@ -1,29 +1,47 @@
-# def mi(a, b):
-   # a = a
-    # b = b / 100
-    # return (a + (a * b))
+def verificar_forca_senha(senha):
+    comprimento_minimo = 8
+    tem_letra_maiuscula = False
+    tem_letra_minuscula = False
+    tem_numero = False
+    tem_caractere_especial = False
 
+    # Verificando o comprimento da senha
+    if len(senha) < comprimento_minimo:
+        return f"Sua senha e muito curta. Recomenda-se no minimo {comprimento_minimo} caracteres."
+   
+    # Verificando se a senha contém sequências comuns
+    sequencias_comuns = ["123456", "abcdef"]
+    for sequencia in sequencias_comuns:
+        if sequencia in senha:
+            return "Sua senha contém uma sequência comum. Tente uma senha mais complexa."
+      
 
-# capacidade_atual, aumento_percentual = int(input("Digite um valor"))
+    # Verificando se a senha contém palavras comuns
+    palavras_comuns = ["password", "123456", "qwerty"]
+    if senha in palavras_comuns:
+        return "Sua senha é muito comum. Tente uma senha mais complexa."
 
-# mi(capacidade_atual, aumento_percentual)
+    # TODO: Verificar se a senha contém letras maiúsculas e minúsculas
+    for char in senha:
+        if char.isupper():
+            tem_letra_maiuscula = True
+        elif char.islower():
+            tem_letra_minuscula = True
 
-#// TODO: Calcule a nova capacidade do disco de Mithril
+    if not (tem_letra_maiuscula and tem_letra_minuscula):
+        return "Sua senha deve conter pelo menos uma letra maiúscula e uma letra minúscula."
 
-#// TODO: Imprima a nova capacidade
+    # TODO: Verificar se a senha contém números
 
-#def map(int, b)
-   # int = 
-#capacidade_atual = int(input(".")) 
-#aumento_percentual = int(input("."))
+    # TODO: Verificar se a senha contém caracteres especiais
 
-#def map(a, b):
- #   global capacidade_atual, aumento_percentual
-    
-  #  a = a
-   ##print (int(a + (a * b)))
+    return "Sua senha atende aos requisitos de seguranca. Parabens!"
 
-#map(capacidade_atual, aumento_percentual)
+# Obtendo a senha do usuário
+senha = input().strip()
 
-capacidade_atual, aumento_percentual = map(int, input(".").split())
-print(int(capacidade_atual + (capacidade_atual * (aumento_percentual / 100))))
+# Verificando a força da senha
+resultado = verificar_forca_senha(senha)
+
+# Imprimindo o resultado
+print(resultado)
