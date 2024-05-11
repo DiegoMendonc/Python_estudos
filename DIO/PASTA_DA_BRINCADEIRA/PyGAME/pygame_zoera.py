eb3e2690d27cc514444c3b6d81b6acd730cf7cd6
DiegoMendonc <diego_felipe_g.m@live.com>
2024-05-11 10:40:40 -0300

    Create pygame_zoera.py

---
+++ DIO/PASTA_DA_BRINCADEIRA/PyGAME/pygame_zoera.py
@@ -0,0 +1,18 @@
+import pygame as pg
+import sys
+
+pg.init()
+
+# Configuração de tela:
+WIDHT = 800
+HEIGHT = 600
+WINDOW_SIZE = (WIDHT, HEIGHT)
+window = pg.display.set_caption("\033[3;31mCLIQUE PARA DESLIGAR O SISTEMA OPERACIONAL!!\033[m")
+
+# CORES:
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 200, 0)
+
+# Propriedades do balão:
