246bf47ffcc7367f48cc07f24ed1a6d3a08d18b1
DiegoMendonc <diego_felipe_g.m@live.com>
2024-05-16 23:33:13 -0300

    sql-dodia

--- DIO/python_db_api/01_dbapi.py
+++ DIO/python_db_api/01_dbapi.py
@@ -18,7 +18,7 @@ def INSERT(conn, cursor, nome, email):
 
 def UPDATE(conn, cursor, nome, email, id):
     data = (nome, email, id)
-    cursor.execute("UPDATE clientes SET nome=?, email=? WHERE id=?;", data)
+    cursor.execute("UPDATE clientes SET profissao=?, salario=? WHERE id=?;", data)
     conn.commit()
 
 def DELETE(conn, cursor, id):
@@ -27,9 +27,22 @@ def DELETE(conn, cursor, id):
     conn.commit()
 
 def INSERT_MANY(conn, cursor, dados):
-    cursor.executemany("INSERT INTO clientes (nome, email) VALUES (?,?);", dados)
+    cursor.executemany("INSERT INTO clientes (profissao, salario) VALUES (?,?);", dados)
     conn.commit()
-    
-dados = [((input("NOME: "), input("EMAIL: "))), (input("NOME: "), input("EMAIL: ")), (input("NOME: "), input("EMAIL: ")), (input("NOME: "), input("EMAIL: "))]
 
-INSERT_MANY(conn, cursor, dados)
+
+# dados = [((input("PROFISSAO: "), input("SALARIO: ")))]
+
+# UPDATE(conn, cursor, "analista administrativo", 3200, 2)
+# UPDATE(conn, cursor, "operador de produção", 2500, 3)
+# UPDATE(conn, cursor, "supervisor de produção", 5000, 4)
+# UPDATE(conn, cursor, "assistente de faturamento", 2300, 5)
+# UPDATE(conn, cursor, "auxiliar administrativo", 1900, 6)
+# UPDATE(conn, cursor, "encarregado operacional", 3500, 7)
+# UPDATE(conn, cursor, "analista de rotas", 5500, 8)
+# UPDATE(conn, cursor, "engenheiro de dados", 9000, 9)
+# UPDATE(conn, cursor, "engenheiro de produção", 8500, 10)
+# UPDATE(conn, cursor, "analista de logística", 2900, 11)
+
+# UPDATE(conn, cursor, "Ana Beatriz Carvalho Rosa", "abcr@outlook.com.br", 12)
+# UPDATE(conn, cursor, "Janaína Godoi", "janadoi@hotmail.com", 14)
